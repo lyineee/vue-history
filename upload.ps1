e86a90c37c96d7c189d4049af92d34e789711830
lyine <liuzhengyuan@hotmail.com>
2021-07-29 19:27:14 +0800

    add upload script

--- upload.ps1
+++ upload.ps1
@@ -1,6 +1,8 @@
 $remotePath = "/www/history";
 $composeFilePath = "~/dev/"
 $fileList = "./dist/*"
+yarn build
 foreach ($item in $fileList) {
     scp -r $item server2:$remotePath;
 }
+ssh server2 docker exec nginx nginx -s reload

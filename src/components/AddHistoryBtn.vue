b3979ace93ad3ee2797ca8479a4bac8445c63313
lyine <liuzhengyuan@hotmail.com>
2022-01-18 21:15:44 +0800

    feat: add button to add new history

---
+++ src/components/AddHistoryBtn.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-dialog v-model="dialog" width="500">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        id="newButton"
+        class="new-history mx-2"
+        fab
+        dark
+        :autofocus="true"
+        v-bind="attrs"
+        v-on="on"
+        color="indigo"
+      >
+        <v-icon dark> mdi-plus </v-icon>
+      </v-btn>
+    </template>
+
+    <v-card>
+      <v-card-title class="text-h5 grey lighten-2"> 输入链接 </v-card-title>
+
+      <v-text-field
+        :loading="btnLoading"
+        clearable
+        label="链接"
+        outlined
+        class="upload-textbox ma-4 mb-0"
+        v-model="updateUrl"
+      ></v-text-field>
+
+      <v-divider></v-divider>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="primary" text @click="addHistory"> 上传 </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script  lang="ts">
+import { environment } from "@/environments/environment";
+import axios, { AxiosError } from "axios";
+import { Vue, Component, Watch, Ref } from "vue-property-decorator";
+@Component
+export default class AddHistoryBtn extends Vue {
+  dialog = false;
+  btnLoading = false;
+  updateUrl = "";
+  logObject(obj: any): any {
+    console.log("testesste");
+    console.log(obj);
+    return obj;
+  }
+  addHistory() {
+    this.btnLoading = true;
+    axios
+      .get(
+        `${environment.apiUrl}/history/update?url=${encodeURIComponent(
+          this.updateUrl
+        )}`
+      )
+      .then(() => {
+        this.$root.$emit("message", "添加成功");
+        this.updateUrl = ""
+        this.dialog = false;
+      })
+      .catch((err: AxiosError) => {
+        console.log(err);
+        this.$root.$emit("message", err.response?.data.message);
+      })
+      .finally(() => {
+        this.btnLoading = false;
+      });
+  }
+}
+</script>
+
+<style>
+#newButton {
+  position: fixed;
+  bottom: 5%;
+  right: 2%;
+}
+</style>
